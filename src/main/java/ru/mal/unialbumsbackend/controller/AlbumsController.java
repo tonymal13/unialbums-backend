9f3654a399de14dd88239c75326640b679a33b1e
Anton Malanin <antonmalanin100@gmail.com>
2024-04-14 19:12:56 +0300

    #2 Refactored a little

--- src/main/java/ru/mal/unialbumsbackend/controller/AlbumsController.java
+++ src/main/java/ru/mal/unialbumsbackend/controller/AlbumsController.java
@@ -64,7 +64,6 @@ public class AlbumsController {
          universeResponse.setData(new ArrayList<>());
          universeResponse.setMessage("Альбомы пользователя:");
 
-
         for (AlbumResponse album : albums) {
             HashMap<String, String> map = new HashMap<>();
             universeResponse.addMap(map);
