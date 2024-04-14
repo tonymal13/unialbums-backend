b033af541d5d21aff5a083b8677d7dffba28b9b0
Anton Malanin <antonmalanin100@gmail.com>
2024-04-14 19:15:34 +0300

    #2 Refactored a little

--- src/main/java/ru/mal/unialbumsbackend/controller/AuthController.java
+++ src/main/java/ru/mal/unialbumsbackend/controller/AuthController.java
@@ -121,9 +121,9 @@ public class AuthController {
         return message;
     }
 
-    @GetMapping("/test")
-    public String test(){
-        return "It's ok";
-    }
+//    @GetMapping("/test")
+//    public String test(){
+//        return "It's ok";
+//    }
 
 }
