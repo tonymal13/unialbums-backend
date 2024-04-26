8478a4b044683ff73b93e1a825c900156ad89870
Anton Malanin <antonmalanin100@gmail.com>
2024-04-26 23:22:55 +0300

    Deleted extra class

--- src/main/java/ru/mal/unialbumsbackend/web/controller/UserController.java
+++ src/main/java/ru/mal/unialbumsbackend/web/controller/UserController.java
@@ -2,13 +2,14 @@ package ru.mal.unialbumsbackend.web.controller;
 
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.mal.unialbumsbackend.domain.User;
 import ru.mal.unialbumsbackend.exception.AuthException;
+import ru.mal.unialbumsbackend.exception.ImageUploadException;
+import ru.mal.unialbumsbackend.repositories.UserRepository;
 import ru.mal.unialbumsbackend.service.ImageService;
 import ru.mal.unialbumsbackend.service.UserService;
 import ru.mal.unialbumsbackend.web.dto.UniverseResponse;
@@ -25,6 +26,10 @@ import static ru.mal.unialbumsbackend.web.controller.AlbumsController.decodeJWTG
 public class UserController {
 
     private final UserService userService;
+
+    private final ImageService imageService;
+
+    private final UserRepository userRepository;
 
     @GetMapping("/myProfile")
     public ResponseEntity<UniverseResponse> getProfile(@RequestHeader(name = "Authorization") String jwt){
@@ -58,6 +63,34 @@ public class UserController {
 
         return ResponseEntity.ok(response);
 
+    }
+
+    @PostMapping("/addAvatar")
+    public void addAvatar(@RequestHeader("Authorization") String jwt ,@RequestParam("avatar") MultipartFile avatar
+    ) {
+
+        JSONObject jsonObject = decodeJWTGetHeader(jwt);
+
+        long userId = ((Number) jsonObject.get("userId")).longValue();
+
+
+
+        String filename= imageService.upload(avatar);
+
+        Optional<User> user=userService.findById(userId);
+        if (user.isPresent()) {
+            user.get().setAvatar("http://localhost:9000/images/" + filename);
+//            user.get().setAvatar("http://79.174.95.140:9000/images/"+filename);
+            userRepository.save(user.get());
+        }
+
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<UniverseResponse> handleException(ImageUploadException e){
+        UniverseResponse universeResponse=new UniverseResponse();
+        universeResponse.setMessage("Не удалось загрузить изображение");
+        return new ResponseEntity<>(universeResponse, HttpStatus.NOT_FOUND);
     }
 
 }
