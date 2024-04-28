b9f4cfe7ddba1586111457c18b8482f738cc23e8
Anton Malanin <antonmalanin100@gmail.com>
2024-04-28 18:53:53 +0300

    Fixed validation

--- src/main/java/ru/mal/unialbumsbackend/util/UserValidator.java
+++ src/main/java/ru/mal/unialbumsbackend/util/UserValidator.java
@@ -25,26 +25,9 @@ public class UserValidator {
         }
         String regex = "\\p{Lu}\\p{L}{1,20}";
 
-        if (userDto.getPassword().length() < 1 || userDto.getPassword().length() > 20)
-            message = "Пароль должен быть от 1 до 20 символов :)";
-        else if(userDto.getUsername().length()<1)
-            message="Логин должен больше 1 до 20 символов :)";
-        else if(!userDto.getFirstName().matches(regex))
-            message="Имя должно быть в формате: Иван";
-        else if(!userDto.getLastName().matches(regex))
-            message="Фамилия должна быть в формате: Иванов";
-        return message;
-    }
-
-    public String validateForEdit(UserDto userDto){
-        String message= "";
-
-        String regex = "\\p{Lu}\\p{L}{1,20}";
-
         if (userDto.getPassword()!=null) {
-
             if (userDto.getPassword().length() < 1 || userDto.getPassword().length() > 20)
-                message = "Пароль должен быть от 1 до 20 символов :)";
+            message = "Пароль должен быть от 1 до 20 символов :)";
         }
         else if(userDto.getUsername().length()<1)
             message="Логин должен больше 1 до 20 символов :)";
@@ -52,9 +35,6 @@ public class UserValidator {
             message="Имя должно быть в формате: Иван";
         else if(!userDto.getLastName().matches(regex))
             message="Фамилия должна быть в формате: Иванов";
-        else{
-            message="Данные успешно обновлены";
-        }
         return message;
     }
 
--- src/main/java/ru/mal/unialbumsbackend/web/controller/UserController.java
+++ src/main/java/ru/mal/unialbumsbackend/web/controller/UserController.java
@@ -71,7 +71,7 @@ public class UserController {
 
         Optional<User> user=userService.findById(userId);
 
-        String message= userValidator.validateForEdit(request);
+        String message= userValidator.validateForRegister(request);
 
         if (message.equals("Данные успешно обновлены")){
             response.setMessage(message);
